12b0973b4f65a7c45f72417f6b610bae78fc67dd
Mandarin <zurual@mail.ru>
2023-09-14 20:12:10 +0300

    game_sc

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-import  pygame
+import pygame
 import pygame.freetype
+from my_car import MyCar
 
 
 pygame.init()
 
 clock = pygame.time.Clock()
 
-screen = pygame.display.set_mode(500, 800)
+screen = pygame.display.set_mode((500, 800))
 pygame.display.set_caption('NFS_lite')
 background_color = (0, 0, 0)
 
@@ -16,7 +17,15 @@ def get_car_image(filename, size, angle):
     image = pygame.transform.rotate(image, angle)
     return image
 
-my_car_image = get_car_image( filename: 'images/mercedes.png', size: (50, 50), -90)
+my_car_image = get_car_image('images/mercedes.png', (50, 50), -90)
 
+my_car = MyCar((250, 600), my_car_image)
 running = True
 while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill(background_color)
+    my_car.draw(screen)
+    pygame.display.flip()
+    clock.tick(60)
--- my_car.py
+++ my_car.py
@@ -1,2 +1,8 @@
 class MyCar:
-    def
+    def __init__(self, position, image):
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.center = position
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
